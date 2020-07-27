851ad87f24fe6a7cd7279591c9f152c9539b36fb
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-27 12:06:11 +0200

    configure the latest version of jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
+// setup file
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow, mount, render } from 'enzyme';
+import App from './App'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+
 
 test('renders without error', () => {
-    render(<App />);
+  // const div = document.createElement('div');
+  render(<App />);
+  // ReactDOM.unmountComponentAtNode(div);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
+  // throw new Error;
 });
