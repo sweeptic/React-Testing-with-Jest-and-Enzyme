5b6b8987d2933bbc274554081cf400be98863fe1
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-27 14:03:14 +0200

    3. Set up Enzyme and Write Tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-     {/* {title}  */}
-     hello
+export default class App extends Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
+  onChangeHandler = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: '43px'
+          }}>The count is {this.state.counter}</h1>
+
+        <button
+          onClick={this.onChangeHandler}
+          style={{
+            display: 'block',
+            margin: '0 auto',
+            fontSize: '25px'
+          }} >Increment Counter</button>
+      </div>
+    )
+  }
 }
 
 
-export default App;
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   const initialState = 0;
+//   const [state, setstate] = useState(initialState)
+
+
+//   return (
+//     <div>
+
+//       <h1 style={{ textAlign: "center", fontSize: '43px' }}>The count is {state}</h1>
+//       <button onClick={() => setstate(state + 1)} style={{ display: 'block', margin: '0 auto', fontSize: '25px' }} >Increment Counter</button>
+//     </div>
+//   )
+// }
+
+// export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,37 @@
 // setup file
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { configure, shallow, mount, render } from 'enzyme';
 import App from './App'
-import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-test('renders without error', () => {
+test('renders without error ', () => {
 
-  const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
+})
 
-  //tbt
-  expect(wrapper).toBeTruthy();
 
-  //tbf
-  // expect(wrapper).toBeFalsy();
-});
+test('renders increment button ', () => {
+
+})
+
+
+test('renders counter display ', () => {
+
+})
+
+test('counter starts at 0 ', () => {
+
+})
+
+
+test('clicking button increments counter display', () => {
+
+})
+
+
+// test('renders without error', () => {
+//   const wrapper = shallow(<App />);
+//   console.log(wrapper.debug());
+//   expect(wrapper).toBeTruthy();
+// });
