5bb7bae8ecd7ce92bdfbcefd500393669fb08cb3
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-28 14:06:42 +0200

    9. Test Button Click

--- src/App.test.js
+++ src/App.test.js
@@ -10,11 +10,13 @@ configure({ adapter: new Adapter() });
 * Factory function to create ShallowWrapper for the App component
 * @function setup
 * @param {object} props - Component props specific to this setup
-* @param {any} state - Initial state for setup.
+* @param {object} state - Initial state for setup.
 * @returns {ShallowWrapper}
 */
 const setup = (props = {}, state = null) => {
-  return shallow(<App {...props} />)
+  const wrapper = shallow(<App {...props} />)
+  if (state) wrapper.setState(state);
+  return wrapper;
 }
 
 /** 
@@ -56,7 +58,12 @@ test('counter starts at 0 ', () => {
 
 
 test('clicking button increments counter display', () => {
-
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
 })
 
 
